386f2853a72b6d37641b9eb5208753ada90ca51d
Karim Zerman <100965013+zermankarim@users.noreply.github.com>
2024-07-14 00:00:25 +0300

    Lift up NavigationContainer

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,12 +3,20 @@ import { store } from "./core/store/store";
 import { FC } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import RootNavigation from "./RootNavigation";
+import { palette } from "./shared/palette";
+import { NavigationContainer } from "@react-navigation/native";
 
 const App: FC = () => {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <RootNavigation></RootNavigation>
+      <SafeAreaProvider
+        style={{
+          backgroundColor: palette.dark[700],
+        }}
+      >
+        <NavigationContainer>
+          <RootNavigation></RootNavigation>
+        </NavigationContainer>
       </SafeAreaProvider>
     </Provider>
   );
