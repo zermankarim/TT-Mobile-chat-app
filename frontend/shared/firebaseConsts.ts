dec2809b38267c31df72564bda379bd51b9d0b3b
Karim Zerman <100965013+zermankarim@users.noreply.github.com>
2024-07-14 00:19:05 +0300

    Delete frontend/shared/firebaseConsts.ts

--- frontend/shared/firebaseConsts.ts
+++
@@ -1,7 +0,0 @@
-export const API_KEY: string = "AIzaSyACV1bPiXLJgBQkFAdtwWU_c--SlrAPjhs";
-export const AUTRH_DOMAIN: string = "tt-mobile-chat-app.firebaseapp.com";
-export const PROJECT_ID: string = "tt-mobile-chat-app";
-export const STORAGE_BUCKET: string = "tt-mobile-chat-app.appspot.com";
-export const MESSAGING_SENDER_ID: string = "1015146140657";
-export const APP_ID: string = "1:1015146140657:web:d730d2472161581cfbea50";
-export const MEASUREMENT_ID: string = "G-XDRF2XMGZM";
