e56eab5769b6e574d740b81e989bb0fdb7d11085
Karim Zerman <100965013+zermankarim@users.noreply.github.com>
2024-07-14 00:02:28 +0300

    Add types and interfaces for new items

--- frontend/shared/types.ts
+++ frontend/shared/types.ts
@@ -1,4 +1,3 @@
-import { RouteProp } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 
 export interface ICounterState {
@@ -15,17 +14,96 @@ export interface ILoginInputsState {
   password: string | null;
 }
 
-type RootStackParamList = {
+export type RootStackParamList = {
   Chat: undefined;
+  Home: undefined;
   Login: undefined;
   SignUp: undefined;
+  CreateChat: undefined;
 };
 
-type LoginScreenNavigationProp = StackNavigationProp<
+export type LoginScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
   "Login"
+>;
+export type ChatScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  "Chat"
+>;
+export type CreateChatScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  "CreateChat"
+>;
+export type SignUpScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  "SignUp"
+>;
+export type HomeScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  "Home"
 >;
 
 export type RouteProps = {
   navigation: LoginScreenNavigationProp;
 };
+
+export interface IChat {
+  _id: string;
+  createdAt: string;
+  createdBy: string;
+  messages: IMessage[];
+  senderEmail: string;
+  recipientEmail: string;
+}
+
+export interface IMessage {
+  _id: string;
+  createdAt: string;
+  text: string;
+  senderEmail: string;
+  recipientEmail: string;
+}
+
+export interface IButtonsList {
+  title: string;
+  icon: React.ReactNode | null;
+  onPress: () => void;
+}
+
+export interface IBottomSheetComponentProps {
+  buttonsList: IButtonsList[];
+}
+
+export interface IPalette {
+  dark: {
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
+    1000: string;
+  };
+  light: {
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
+    1000: string;
+  };
+  blue: {
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+  };
+}
