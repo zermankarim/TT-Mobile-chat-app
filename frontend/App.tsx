da40b1cc43522fcd6dad89d0858bcae5d739719a
Karim Zerman <100965013+zermankarim@users.noreply.github.com>
2024-07-17 23:41:14 +0300

    Rewrite logic for loading fonts

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,25 +1,46 @@
 import { Provider } from "react-redux";
 import { store } from "./core/store/store";
-import { FC } from "react";
+import { FC, useCallback, useEffect, useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import RootNavigation from "./RootNavigation";
 import { palette } from "./shared/palette";
 import { NavigationContainer } from "@react-navigation/native";
-import AppLoading from "expo-app-loading";
-import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
 
 const App: FC = () => {
-  // Adding new fonts
-  const [fontsLoaded] = useFonts({
-    "cabin-regular": require("./assets/fonts/Cabin-Regular.ttf"),
-  });
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  const [appIsReady, setAppIsReady] = useState(false);
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          "cabin-regular": require("./assets/fonts/Cabin-Regular.ttf"),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
 
   return (
     <Provider store={store}>
       <SafeAreaProvider
+        onLayout={onLayoutRootView}
         style={{
           backgroundColor: palette.dark[700],
         }}
