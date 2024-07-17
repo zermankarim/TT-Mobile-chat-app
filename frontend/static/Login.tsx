fa28d35249516d2a0889e962a69d7edd14ede007
Karim Zerman <100965013+zermankarim@users.noreply.github.com>
2024-07-17 20:54:31 +0300

    Add logic for password reset

--- frontend/static/Login.tsx
+++ frontend/static/Login.tsx
@@ -1,7 +1,6 @@
 import {
   ActivityIndicator,
   Alert,
-  ImageBackground,
   NativeSyntheticEvent,
   TextInputChangeEventData,
   View,
@@ -15,22 +14,33 @@ import { IUserState, RouteProps } from "../shared/types";
 import { palette } from "../shared/palette";
 import { doc, getDoc } from "firebase/firestore";
 import TextWithFont from "../shared/components/TextWithFont";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { Overlay } from "@rneui/themed";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Login: FC<RouteProps> = ({ navigation }) => {
   // Redux states and dispatch
   const dispatch = useDispatch();
 
   // States
+  const [isVisibleOverlay, setIsVisibleOverlay] = useState<boolean>(false);
+  const [isDisabledButton, setIsDisabledButton] = useState<boolean>(false);
+
   const [loadingLogin, setLoadingLogin] = useState<boolean>(false);
+
   const [email, setEmail] = useState<string | null>(null);
   const [password, setPassword] = useState<string | null>(null);
+
   const [showEmailError, setShowEmailError] =
     useState<SetStateAction<boolean>>(false);
   const [showPasswordError, setShowPasswordError] =
     useState<SetStateAction<boolean>>(false);
 
-  const handleLoginChange = (
+  // Functions
+  const handleEmailChange = (
     e: NativeSyntheticEvent<TextInputChangeEventData>
   ) => {
     if (!e.nativeEvent.text) {
@@ -55,6 +65,7 @@ const Login: FC<RouteProps> = ({ navigation }) => {
   };
 
   const onHandleLogin = async () => {
+    setIsDisabledButton(true);
     setLoadingLogin(true);
     if (!email || !password) {
       Alert.alert("All fields must be filled in");
@@ -77,12 +88,40 @@ const Login: FC<RouteProps> = ({ navigation }) => {
         console.error("No such document!");
       }
       setLoadingLogin(false);
+      setIsDisabledButton(false);
     } catch (e: any) {
       Alert.alert("Error during login user: ", e.message);
       setLoadingLogin(false);
+      setIsDisabledButton(false);
     }
   };
 
+  const handleResetPassword = async (email: string | null) => {
+    setIsDisabledButton(true);
+    if (email) {
+      sendPasswordResetEmail(auth, email)
+        .then(() => {
+          // Password reset email sent!
+          setEmail(null);
+          setIsDisabledButton(false);
+          setIsVisibleOverlay(false);
+          Alert.alert("Password reset information sent successfully!");
+        })
+        .catch((error) => {
+          setIsDisabledButton(false);
+          setEmail(null);
+          setIsVisibleOverlay(false);
+          const errorMessage = error.message;
+
+          Alert.alert("Eror during sending password reset info", errorMessage);
+          console.error(
+            "Eror during sending password reset info",
+            errorMessage
+          );
+          // ..
+        });
+    }
+  };
   if (loadingLogin) {
     return (
       <ActivityIndicator
@@ -102,126 +141,244 @@ const Login: FC<RouteProps> = ({ navigation }) => {
         flex: 1,
         justifyContent: "center",
         backgroundColor: palette.dark[700],
+        padding: 12,
       }}
     >
-      <ImageBackground
-        source={require("../assets/background-image-login-signup.jpg")}
-        resizeMode="cover"
-        blurRadius={1}
+      <View // Inner container
         style={{
-          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 12,
+          backgroundColor: palette.dark[500],
+          borderColor: palette.dark[200],
+          borderWidth: 1,
+          borderRadius: 12,
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 6,
+          },
+          shadowOpacity: 0.23,
+          shadowRadius: 8.3,
+          elevation: 10,
         }}
       >
-        <View // Inner container
+        <TextWithFont
+          styleProps={{
+            fontSize: 30,
+            color: "white",
+          }}
+        >
+          Log In
+        </TextWithFont>
+        <View
           style={{
-            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            marginTop: 20,
+            width: "100%",
+          }}
+        >
+          <Input
+            placeholder="Email"
+            onChange={handleEmailChange}
+            errorStyle={{ color: "red" }}
+            style={{
+              color: "white",
+            }}
+            errorMessage={
+              !email && showEmailError ? "This field must be fill" : ""
+            }
+          ></Input>
+          <Input
+            placeholder="Password"
+            onChange={handlePasswordChange}
+            errorStyle={{ color: "red" }}
+            style={{
+              color: "white",
+            }}
+            secureTextEntry={true}
+            errorMessage={
+              !password && showPasswordError ? "This field must be fill" : ""
+            }
+          ></Input>
+        </View>
+
+        <Button
+          title="Log In"
+          onPress={onHandleLogin}
+          loading={isDisabledButton}
+          disabled={isDisabledButton}
+          disabledStyle={{
+            backgroundColor: palette.light[300],
+          }}
+          loadingProps={{ size: "small", color: "white" }}
+          buttonStyle={{
+            backgroundColor: palette.blue[200],
+            borderRadius: 5,
+            width: "100%",
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 6,
+            },
+            shadowOpacity: 0.23,
+            shadowRadius: 8.3,
+            elevation: 10,
+          }}
+          titleStyle={{ fontWeight: "bold", fontSize: 16 }}
+          containerStyle={{
+            marginHorizontal: 50,
+            height: 50,
+            marginVertical: 10,
+            width: "100%",
+          }}
+        />
+        <View
+          style={{
+            display: "flex",
             justifyContent: "center",
-            // backgroundColor: palette.dark[900],
             alignItems: "center",
-            padding: 10,
+            flexDirection: "row",
+            gap: 4,
+            width: "100%",
           }}
         >
           <TextWithFont
             styleProps={{
-              fontSize: 30,
               color: "white",
             }}
           >
-            Login
+            Don't have an account?
           </TextWithFont>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              marginTop: 20,
-              width: "100%",
-            }}
-          >
-            <Input
-              placeholder="Login"
-              onChange={handleLoginChange}
-              errorStyle={{ color: "red" }}
-              style={{
-                color: "white",
-              }}
-              errorMessage={
-                !email && showEmailError ? "This field must be fill" : ""
-              }
-            ></Input>
-            <Input
-              placeholder="Password"
-              onChange={handlePasswordChange}
-              errorStyle={{ color: "red" }}
-              style={{
-                color: "white",
-              }}
-              secureTextEntry={true}
-              errorMessage={
-                !password && showPasswordError ? "This field must be fill" : ""
-              }
-            ></Input>
-          </View>
-
           <Button
-            title="Log in"
-            onPress={onHandleLogin}
-            loading={false}
-            loadingProps={{ size: "small", color: "white" }}
+            onPress={() => navigation.navigate("SignUp")}
             buttonStyle={{
-              backgroundColor: palette.blue[200],
-              borderRadius: 5,
-              width: "100%",
-              shadowColor: "#000",
-              shadowOffset: {
-                width: 0,
-                height: 6,
-              },
-              shadowOpacity: 0.23,
-              shadowRadius: 8.3,
-              elevation: 10,
-            }}
-            titleStyle={{ fontWeight: "bold", fontSize: 16 }}
-            containerStyle={{
-              marginHorizontal: 50,
-              height: 50,
-              marginVertical: 10,
-              width: "100%",
-            }}
-          />
-          <View
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "row",
-              gap: 8,
-              width: "100%",
+              backgroundColor: "transparent",
             }}
           >
             <TextWithFont
               styleProps={{
-                color: "white",
+                color: palette.blue[100],
               }}
             >
-              Don't have an account?
+              Sign Up
             </TextWithFont>
-            <Button
-              onPress={() => navigation.navigate("SignUp")}
-              buttonStyle={{
-                backgroundColor: "transparent",
+          </Button>
+        </View>
+        <View
+          style={{
+            borderWidth: 0.5,
+            borderColor: palette.dark[100],
+            width: "100%",
+          }}
+        ></View>
+        <View
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "row",
+            gap: 4,
+            width: "100%",
+          }}
+        >
+          <TextWithFont
+            styleProps={{
+              color: "white",
+            }}
+          >
+            Forgot your password?
+          </TextWithFont>
+          <Button
+            onPress={() => setIsVisibleOverlay(true)}
+            buttonStyle={{
+              backgroundColor: "transparent",
+            }}
+          >
+            <TextWithFont
+              styleProps={{
+                color: palette.blue[100],
               }}
             >
-              <TextWithFont
-                styleProps={{
-                  color: palette.blue[100],
-                }}
-              >
-                Sign Up
-              </TextWithFont>
-            </Button>
-          </View>
+              Reset
+            </TextWithFont>
+          </Button>
         </View>
-      </ImageBackground>
+      </View>
+      <Overlay
+        isVisible={isVisibleOverlay}
+        onBackdropPress={() => {
+          setIsVisibleOverlay(false);
+          setEmail(null);
+          setIsDisabledButton(false);
+        }}
+        overlayStyle={{
+          display: "flex",
+          alignItems: "center",
+          gap: 12,
+          width: "90%",
+          borderRadius: 12,
+          backgroundColor: palette.dark[100],
+          borderWidth: 1,
+          borderColor: palette.light[100],
+          padding: 12,
+        }}
+      >
+        <MaterialIcons name="lock-reset" size={64} color={palette.light[700]} />
+        <TextWithFont
+          styleProps={{
+            fontSize: 16,
+            width: "100%",
+            textAlign: "center",
+            color: palette.light[1000],
+          }}
+        >
+          We'll send your password reset info to the email address linked to you
+          acconut.
+        </TextWithFont>
+        <Input
+          placeholder="Email"
+          onChange={handleEmailChange}
+          errorStyle={{ color: "red" }}
+          style={{
+            color: "white",
+          }}
+          errorMessage={
+            !password && showPasswordError ? "This field must be fill" : ""
+          }
+        ></Input>
+        <Button
+          title="Reset password"
+          onPress={() => handleResetPassword(email)}
+          loading={isDisabledButton}
+          disabled={isDisabledButton}
+          disabledStyle={{
+            backgroundColor: palette.light[300],
+          }}
+          loadingProps={{ size: "small", color: "white" }}
+          buttonStyle={{
+            backgroundColor: palette.blue[200],
+            borderRadius: 5,
+            width: "100%",
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 6,
+            },
+            shadowOpacity: 0.23,
+            shadowRadius: 8.3,
+            elevation: 4,
+          }}
+          titleStyle={{ fontWeight: "bold", fontSize: 16 }}
+          containerStyle={{
+            marginHorizontal: 50,
+            height: 50,
+            marginVertical: 10,
+            width: "100%",
+          }}
+        />
+      </Overlay>
     </View>
   );
 };
